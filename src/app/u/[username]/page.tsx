1490d9866d0482810f527f25f7ca8163e77f57e1
Divya Patel <divyapatel6514@gmail.com>
2025-03-15 22:29:29 +0530

    updated

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -65,13 +65,10 @@ export default function SendMessage() {
       // Send feedback to analysis API
       const analysisResponse = await axios.post(
           "https://silent-iq.vercel.app/api/analyze-feedback",
-          {
-            message: data.content, // Pass JSON payload correctly
-          },
-          {
-            headers: { "Content-Type": "application/json" },
-          }
+          { message: data.content },
+          { headers: { "Content-Type": "application/json" } }
       );
+
 
       const sentimentData = analysisResponse.data;
       // Save feedback with sentiment analysis
